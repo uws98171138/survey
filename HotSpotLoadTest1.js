fecc74a7d0d9b1e8d51b12e72c806de7ef7d4f68
uws98171138 <a.evangelista@westernsydney.edu.au>
2016-02-25 11:10:36 +1100

    Delete HotSpotLoadTest1.js

--- HotSpotLoadTest1.js
+++
@@ -1,8 +0,0 @@
-var idphotoget9a = imageid();
-var picstatusraw9a = document.getElementById(idphotoget9a).complete;
-var picstatus9a = String(picstatusraw9a);
-var docstatus9araw = document.readyState;
-var docstatus9a = String(docstatus9araw);
-var statuscode9a;
-if (docstatus9a == "interactive" || docstatus9a == "complete") {statuscode9a = 1;} else {statuscode9a = 0;};
-if (picstatus9a == "false" && statuscode9a == 1) {location.reload();};
