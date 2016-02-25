3952e481b84e99710b6ac1666192def6138945cd
uws98171138 <a.evangelista@westernsydney.edu.au>
2016-02-25 11:10:43 +1100

    Delete HotSpotCombinedLoaded4.js

--- HotSpotCombinedLoaded4.js
+++
@@ -1,6 +0,0 @@
-var h1count = document.getElementsByTagName("h1").length;
-if (h1count > 0) {
-document.getElementById("skinner").setAttribute("id", "skinner");
-document.getElementById("qtext0").setAttribute("id", "qtext0L4");
-document.getElementsByTagName("html")[0].id = "loaded4";
-document.getElementsByTagName("div")[14].id = "HeadError";};
