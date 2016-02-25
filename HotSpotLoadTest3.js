ef8f8dbdd5f4aa6f5a0135de3c1219f978ce6576
uws98171138 <a.evangelista@westernsydney.edu.au>
2016-02-25 11:10:23 +1100

    Delete HotSpotLoadTest3.js

--- HotSpotLoadTest3.js
+++
@@ -1,4 +0,0 @@
-var idphotoget9b = imageid();
-var width9raw = document.getElementById(idphotoget9b).getAttributeNode("width").value;
-var width9 = Number(width9raw);
-if (width9 === 0) {alert("The page is being reloaded as there are problems showing the picture"); location.reload();};
